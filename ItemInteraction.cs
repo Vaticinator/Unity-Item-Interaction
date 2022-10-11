1cf5c5d59dd7609dd616b513deb52d81e47e74ac
Vaticinator <barnaba@cat-astrophe-games.com>
2022-10-11 09:24:38 +0200

    first commit

---
+++ ItemInteraction.cs
@@ -0,0 +1,307 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+/* GOOD TO KNOW:  Hide/Show is disabling/enabling renderers, lights and colliders (not triggers)  */
+
+public class ItemInteraction : MonoBehaviour
+{
+    [SerializeField] public actionTypes actionType;
+    [SerializeField] public actionTriggers actionTrigger;
+    [SerializeField] public bool repeatable = true;
+
+    [SerializeField] public bool additionalCondition = false;
+    [SerializeField] public GameObject conditionObject = null;
+    [SerializeField] public AdditionalConditions condition;
+
+    [SerializeField] public bool delegateAction = false;
+    [SerializeField] public GameObject delegateActionTo = null;
+
+    private bool interactionDisabled = false;
+    private bool isInCollider = false;
+    private Animator itemAnimator;
+    private AudioSource itemAudioSource;
+    private ParticleSystem itemParticles;
+    private Dictionary<string, bool> controlledAnimationParameters = new Dictionary<string, bool>();
+
+    private GameObject targetItem = null;
+
+    public enum actionTypes { AnimationTriggers, Show, Hide, AudioPlay, AudioStop, ParticlesPlay, ParticlesStop }
+    public enum actionTriggers { Default, OnEnter, OnExit, OnKey }
+    public enum AdditionalConditions { IsVisible, IsHidden }
+
+    private actionTypes[] animationGroup = { actionTypes.AnimationTriggers };
+    private actionTypes[] audioGroup = { actionTypes.AudioPlay, actionTypes.AudioStop };
+    private actionTypes[] particlesGroup = { actionTypes.ParticlesPlay, actionTypes.ParticlesStop };
+
+    void Start()
+    {
+        targetItem = (delegateAction && delegateActionTo != null) ? delegateActionTo : gameObject;
+
+        if (animationGroup.Contains(actionType))
+        {
+            if (targetItem.TryGetComponent(out itemAnimator))
+            {
+                controlledAnimationParameters.Add("enter", false);
+                controlledAnimationParameters.Add("exit", false);
+                controlledAnimationParameters.Add("key", false);
+                foreach (AnimatorControllerParameter p in itemAnimator.parameters)
+                {
+                    if (controlledAnimationParameters.ContainsKey(p.name))
+                        controlledAnimationParameters[p.name] = true;
+                }
+            }
+            else
+                disableWithWarning("No Animator in " + targetItem.name + ".");
+        }
+        else if (audioGroup.Contains(actionType))
+        {
+            if (!targetItem.TryGetComponent(out itemAudioSource))
+                disableWithWarning("No AudioSource in " + targetItem.name + ".");
+        }
+        else if (particlesGroup.Contains(actionType))
+        {
+            if (!targetItem.TryGetComponent(out itemParticles))
+                disableWithWarning("No ParticleSystem in " + targetItem.name + ".");
+        }
+
+        if (actionTrigger == actionTriggers.Default)
+            InvokeAction("default");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        OnEnter();
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        OnExit();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnEnter();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnExit();
+    }
+
+    private void Update()
+    {
+        if (interactionDisabled || !isAdditionalConditionPassed())
+            return;
+
+        if (actionTrigger == actionTriggers.OnKey && isInCollider && Input.GetKeyDown(KeyCode.F))
+            InvokeAction("key");
+    }
+
+    private void OnEnter()
+    {
+        isInCollider = true;
+        if (interactionDisabled || !isAdditionalConditionPassed())
+            return;
+
+        if (actionTrigger == actionTriggers.OnEnter || actionType == actionTypes.AnimationTriggers)
+            InvokeAction("enter");
+    }
+
+    private void OnExit()
+    {
+        isInCollider = false;
+        if (interactionDisabled || !isAdditionalConditionPassed())
+            return;
+
+        if (actionTrigger == actionTriggers.OnExit || actionType == actionTypes.AnimationTriggers)
+            InvokeAction("exit");
+    }
+
+    private void InvokeAction(string trigger)
+    {
+        switch (actionType)
+        {
+            case actionTypes.AnimationTriggers:
+                SetAnimatorTrigger(trigger);
+                interactionFinalize();
+                break;
+            case actionTypes.Show:
+                Show();
+                interactionFinalize();
+                break;
+            case actionTypes.Hide:
+                Hide();
+                interactionFinalize();
+                break;
+            case actionTypes.AudioPlay:
+                PlayAudio();
+                interactionFinalize();
+                break;
+            case actionTypes.AudioStop:
+                StopAudio();
+                interactionFinalize();
+                break;
+            case actionTypes.ParticlesPlay:
+                PlayParticles();
+                interactionFinalize();
+                break;
+            case actionTypes.ParticlesStop:
+                StopParticles();
+                interactionFinalize();
+                break;
+        }
+    }
+
+    private void Hide()
+    {
+        foreach (Renderer r in targetItem.GetComponentsInChildren<Renderer>())
+            r.enabled = false;
+        foreach (Light l in targetItem.GetComponentsInChildren<Light>())
+            l.enabled = false;
+        foreach (Collider c in targetItem.GetComponentsInChildren<Collider>())
+            c.enabled = (c.isTrigger) ? c.enabled : false;
+    }
+
+    private void Show()
+    {
+        foreach (Renderer r in targetItem.GetComponentsInChildren<Renderer>())
+            r.enabled = true;
+        foreach (Light l in targetItem.GetComponentsInChildren<Light>())
+            l.enabled = true;
+        foreach (Collider c in targetItem.GetComponentsInChildren<Collider>())
+            c.enabled = (c.isTrigger) ? c.enabled : true;
+    }
+
+    private void PlayAudio()
+    {
+        if (!itemAudioSource.isPlaying)
+            itemAudioSource.Play();
+    }
+
+    private void StopAudio()
+    {
+        itemAudioSource.Stop();
+    }
+
+    private void PlayParticles()
+    {
+        if (!itemParticles.isPlaying)
+            itemParticles.Play();
+    }
+
+    private void StopParticles()
+    {
+        itemParticles.Stop();
+    }
+
+    private void SetAnimatorTrigger(string triggerName)
+    {
+        if (!controlledAnimationParameters[triggerName])
+            return;
+
+        itemAnimator.SetTrigger(triggerName);
+
+        if (!repeatable)
+            controlledAnimationParameters[triggerName] = false;
+    }
+
+    private void interactionFinalize()
+    {
+        if (!repeatable && actionType == actionTypes.AnimationTriggers && !controlledAnimationParameters.ContainsValue(true))
+            interactionDisabled = true;
+            
+        else if (!repeatable && actionType != actionTypes.AnimationTriggers)
+            interactionDisabled = true;
+    }
+
+    private void disableWithWarning(string warning)
+    {
+        interactionDisabled = true;
+        Debug.LogWarning(warning);
+    }
+
+    private bool isAdditionalConditionPassed()
+    {
+        if (!additionalCondition || conditionObject == null)
+            return true;
+
+        bool expectVisible = (condition == AdditionalConditions.IsVisible);
+
+        // check if every renderer are desabled (don't check lights and colliders)
+        foreach (Renderer r in conditionObject.GetComponentsInChildren<Renderer>())
+        {
+            if (r.enabled != expectVisible)
+                return false;
+        }
+        return true;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ItemInteraction))]
+[CanEditMultipleObjects]
+public class ItemInteractionEditor : Editor
+{
+    SerializedProperty actionType;
+    SerializedProperty actionTrigger;
+    SerializedProperty repeatable;
+    SerializedProperty additionalCondition;
+    SerializedProperty conditionObject;
+    SerializedProperty condition;
+    SerializedProperty delegateAction;
+    SerializedProperty delegateActionTo;
+
+    void OnEnable()
+    {        
+        actionType = serializedObject.FindProperty("actionType");
+        actionTrigger = serializedObject.FindProperty("actionTrigger");
+        repeatable = serializedObject.FindProperty("repeatable");
+        additionalCondition = serializedObject.FindProperty("additionalCondition");
+        conditionObject = serializedObject.FindProperty("conditionObject");
+        condition = serializedObject.FindProperty("condition");
+        delegateAction = serializedObject.FindProperty("delegateAction");
+        delegateActionTo = serializedObject.FindProperty("delegateActionTo");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        ItemInteraction itemInteractionComponent = (ItemInteraction)target;
+
+        EditorGUILayout.PropertyField(actionType);
+
+        if (itemInteractionComponent.actionType != ItemInteraction.actionTypes.AnimationTriggers)
+            EditorGUILayout.PropertyField(actionTrigger);
+        else
+            EditorGUILayout.LabelField("Now you can use \"enter\", \"exit\" and \"key\" triggers in the Animator.", EditorStyles.helpBox);
+
+        if (itemInteractionComponent.actionTrigger != ItemInteraction.actionTriggers.Default)
+            EditorGUILayout.PropertyField(repeatable);        
+        else
+            EditorGUILayout.LabelField("-", "Trigger action once when scene is starting.", EditorStyles.helpBox);
+
+        EditorGUILayout.Separator();
+        EditorGUILayout.LabelField("Advanced:", EditorStyles.boldLabel);
+
+        EditorGUILayout.PropertyField(additionalCondition);
+        if (itemInteractionComponent.additionalCondition)
+        {
+            EditorGUILayout.LabelField("Choose additional condition for the action above:", EditorStyles.helpBox);
+            EditorGUILayout.PropertyField(conditionObject);
+            EditorGUILayout.PropertyField(condition);
+            EditorGUILayout.Separator();
+        }
+
+        EditorGUILayout.PropertyField(delegateAction);
+        if (itemInteractionComponent.delegateAction)
+        {
+            EditorGUILayout.LabelField("Trigger action from this object but play on a different object:", EditorStyles.helpBox);
+            EditorGUILayout.PropertyField(delegateActionTo);
+        }
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Unity-Interaction-Component
-Component for basics interactions and very simple game logic
+# Item Interaction Component for Unity
+Component for basics interactions and very simple game logic.
+
+Just drag-and-drop this component to the object with any kind of collider (isTrigger checkbox has to be selected) and you can choose the interaction type from the list.
+
+![Basic Example](illustrations/basic.png)
+
+# Explaination:
+
+**Action Type**
+
+Possible actions are:
+- Animation Triggers (if the Object has an Animator)
+- Hide Object
+- Show Object
+- Play Audio (if the Object has an AudioSource)
+- Stop Audio (if the Object has an AudioSource)
+- Play Particles (if the Object has a ParticleSystem)
+- Stop Particles (if the Object has a ParticleSystem)
+
+*Note for Animation Triggers: In Animator under "parameters" tab, you can add parameters in type of trigger with names: __enter__, __exit__ and __key__, and then use them in conditions of transitions.*
+
+**Action Trigger**
+
+You can chose if you want to trigger action when player enter to "collider" or when player exit from "collider" or when player hit key "F" on the keyboard (beeing in range of "collider").
+
+**Repeatable**
+
+If selected, action will play any time when player enter/leave "collider". In other case it play action once and then is disabled.
+
+**Additional Condition**
+
+With this option you can create some simple logic. For example you can play animation on one object only when you hide different object before... So the door will open when you collect the key.
+
+**Delegate Action**
+
+This option is helpful when you want to play selected action on different object.
+
+**Additional Notes**
+
+You can add many Interaction Components to one object. For example if you want to hide item and play sound, add two of Interaction Components with different settings.
+
+...Hopefully I will add some video examples soon.
+
+![Advanced Example](illustrations/advanced.png)
